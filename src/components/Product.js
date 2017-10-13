9a82f50b764338474c38778d5a527558d54fa34b
Timothy Cevallos <timcev@gmail.com>
2017-10-13 02:29:02 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,11 @@
 // Code Product Component Here
 class Product extends React.Component {
- 
+
   render() {
     return (
       <div className="order">
         <ul>
-          <li>{this.props.cone ? 'Cone' : 'Cup'}</li>
+          <li>{this.props.name}</li>
           <li>{this.props.size}</li>
           <li>{this.props.scoops.length} scoops: {this.props.scoops.join(', ')}</li>
           <li>Ordered by {this.props.orderInfo.customerName} at {this.props.orderInfo.orderedAt}.</li>
