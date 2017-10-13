4ca36fdfee61b597d53c2a0b106a863afbdee37a
Timothy Cevallos <timcev@gmail.com>
2017-10-13 02:30:05 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,7 +7,7 @@ class Product extends React.Component {
         <ul>
           <li>{this.props.name}</li>
           <li>{this.props.producer}</li>
-          <li>{this.props.producer}</li>
+          <li>{this.props.hasWatermark}</li>
           <li>{this.props.color}</li>
           <li>{this.props.weight}</li>
         </ul>
@@ -15,3 +15,4 @@ class Product extends React.Component {
     );
   }
 }
+hasWatermark
