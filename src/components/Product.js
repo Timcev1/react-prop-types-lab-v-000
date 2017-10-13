b89afcdb74f67bca548c0e83beb5327cce5c2407
Timothy Cevallos <timcev@gmail.com>
2017-10-13 02:45:08 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,15 +6,7 @@ class Product extends React.Component {
 
   render() {
     return (
-      <div className="order">
-        <ul>
-          <li>{this.props.name}</li>
-          <li>{this.props.producer}</li>
-          <li>{this.props.hasWatermark ? "Yes" : "No"}</li>
-          <li>{this.props.color}</li>
-          <li>{this.props.weight}</li>
-        </ul>
-      </div>
+      <div>Product </div>
     );
   }
 }
