cca79a2745eadcc316363ab9b4a62a7e1c919ab9
Timothy Cevallos <timcev@gmail.com>
2017-10-13 02:29:31 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,7 +7,7 @@ class Product extends React.Component {
         <ul>
           <li>{this.props.name}</li>
           <li>{this.props.producer}</li>
-          <li>{this.props.scoops.length} scoops: {this.props.scoops.join(', ')}</li>
+          <li>{this.props.color}</li>
           <li>Ordered by {this.props.orderInfo.customerName} at {this.props.orderInfo.orderedAt}.</li>
         </ul>
       </div>
